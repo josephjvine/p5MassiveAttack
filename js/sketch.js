dc41cfc2d98c540c119e785873e02927e3480598
josephjvine <93606160+josephjvine@users.noreply.github.com>
2021-12-10 16:51:07 +0000

    added visuals for angel

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,59 @@
+var mySong, amplitude, play;
+
+function preload(){
+     mySong = loadSound('assets/massiveAttackAngel.mp3');
+}
+
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+ 
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("p5container");
+
+    rectMode(CENTER);
+
+    amplitude = new p5.Amplitude();
+    background(0);
+    play = false;
+    button = createButton("Play");
+    button.addClass('btn');
+    button.center();
+    
+    button.mousePressed(()=>{
+        play = true;
+        mySong.play();
+        button.hide();
+    });
 }
 
 function draw() {
-    background(220);
+    console.log(play);
+    if(play == true){
+            //console.log(amplitude.getLevel());
+    var level = amplitude.getLevel();
+    var vol = map(level, 0, 1, 0, 1000);
+    var col = map(level, 0, 1, 0, 255);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    background(col);
+
+    //console.log(vol);
+
+    noStroke(0);
+
+    if(vol>200){
+
+        fill(col);
+        ellipse(width/2, height/2, vol + 100);
+    }
+
+    fill(col, 100, 100);
+    ellipse(width/2, height/2, vol);
+
+    fill(100, col, 100);
+    ellipse(width/2, height/2, vol/2);
+
+    }
+
+
+
 }
